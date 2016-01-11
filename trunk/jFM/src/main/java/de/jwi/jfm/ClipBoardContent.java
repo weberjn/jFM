9f9df46ec784e958f5ee98664abfc3022ee31a34
weberjn <weberjn@205ee8e7-7b6f-eb40-8847-f897def4af0e>
2016-01-11 19:48:18 +0000

    clipboard

---
+++ trunk/jFM/src/main/java/de/jwi/jfm/ClipBoardContent.java
@@ -0,0 +1,44 @@
+
+package de.jwi.jfm;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Jürgen Weber Source file created on 10.01.2016
+ */
+public class ClipBoardContent {
+
+	public static final int CUT_CONTENT = 1;
+	public static final int COPY_CONTENT = 2;
+	
+	public int contentType;
+	
+	public File[] selectedfiles;
+
+	public ClipBoardContent(int contentType, File[] selectedfiles) {
+		super();
+		this.contentType = contentType;
+		this.selectedfiles = selectedfiles;
+	}
+
+	public String getFileCount()
+	{
+		return "" + selectedfiles.length;
+	}
+	
+	public String getFiles()
+	{
+		List<File> l = Arrays.asList(selectedfiles);
+		String s = l.toString();
+		return s;
+	}
+	
+	public String getKind()
+	{
+		String s = contentType == COPY_CONTENT ? "copy" : "cut";
+		
+		return s;
+	}
+}
